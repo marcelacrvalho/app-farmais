45e523b48c38cc71d4c8994c348d9a65e2e8c70a
marcelacrvalho <marcelacrv@icloud.com>
2023-04-16 16:51:38 -0300

    Adiciona strings que serão utilizadas no app

---
+++ lib/values/strings.dart
@@ -0,0 +1,15 @@
+const title = 'Farmais';
+
+// AuthenticationView
+const hintEmail = 'Insira seu e-mail';
+const hintPassword = 'Escolha sua senha';
+const hintCity = 'Agora, sua cidade';
+const hintStreet = 'Sua rua';
+const hintNeighborhood = 'Seu bairro';
+const hintNumber = 'Número';
+
+// StepView
+const about = 'Se cuidar é uma forma de amar.\nCom o app Farmais, você compra '
+    'seus remédios, com receita ou sem, e recebe na sua casa. Sem precisar '
+    'se estressar em uma fila gigante.';
+const buttonNext = 'Prosseguir';
