ea35a78d9f87b6dde1e577e311bbca4271151369
marcelacrvalho <marcelacrv@icloud.com>
2023-04-16 16:52:34 -0300

    Remove rota da SplashView

--- lib/values/routes.dart
+++ lib/values/routes.dart
@@ -1,7 +1,5 @@
 import '../features/farmais/authentication/presentation/authentication_view.dart';
-import '../features/farmais/splash/presentation/views/splash_view.dart';
 
 final myRoute = {
-  '/': (context) => const MySplashView(),
   '/authentication': (context) => const AuthenticationView(),
 };
