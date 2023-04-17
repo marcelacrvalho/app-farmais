2144d99b09edf1c04e0e91f96029c287fd5206ab
marcelacrvalho <marcelacrv@icloud.com>
2023-04-17 19:51:27 -0300

    Adiciona TextTheme e ButtonTheme

--- lib/values/theme.dart
+++ lib/values/theme.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
 final myTheme = ThemeData(
-  primaryColor: Colors.red,
+  primaryColor: const Color(0XFFC15151),
+  scaffoldBackgroundColor: Colors.white,
+  textTheme: const TextTheme(
+    titleLarge: TextStyle(
+      color: Color(0XFFD2C094),
+      fontSize: 24.0,
+      fontWeight: FontWeight.bold,
+    ),
+    bodyLarge: TextStyle(
+      color: Colors.black54,
+      fontSize: 14.0,
+    ),
+    bodySmall: TextStyle(
+      color: Colors.grey,
+      fontSize: 12.0,
+    ),
+  ),
+
+  buttonTheme: ButtonThemeData(
+    buttonColor: const Color(0XFFC15151),
+    padding: const EdgeInsets.all(16.0),
+    disabledColor: Colors.grey,
+    splashColor: Color(0XFFC15151).withOpacity(0.5),
+  ),
 );
