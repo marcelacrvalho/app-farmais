16b6c7ec6154e5cc37254c4315167ea94d6e1497
marcelacrvalho <marcelacrv@icloud.com>
2023-04-17 19:07:59 -0300

    Inicializa tema

--- lib/values/theme.dart
+++ lib/values/theme.dart
@@ -1,14 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'routes.dart';
-import 'strings.dart';
-
-final myTheme = MaterialApp(
-  debugShowCheckedModeBanner: false,
-  title: title,
-  theme: ThemeData(
-    primaryColor: Colors.red,
-  ),
-  initialRoute: '/',
-  routes: myRoute,
+final myTheme = ThemeData(
+  primaryColor: Colors.red,
 );
