faab5f5a896e3a3dc3dd6a928b99afdef4ee790f
marcelacrvalho <marcelacrv@icloud.com>
2023-04-18 14:53:16 -0300

    Renomeia strings

--- lib/values/strings.dart
+++ lib/values/strings.dart
@@ -1,15 +1,15 @@
 const title = 'Farmais';
 
 // AuthenticationView
-const hintEmail = 'Insira seu e-mail';
-const hintPassword = 'Escolha sua senha';
-const hintCity = 'Agora, sua cidade';
-const hintStreet = 'Sua rua';
-const hintNeighborhood = 'Seu bairro';
-const hintNumber = 'Número';
+const hintEmailAuthenticationView = 'Insira seu e-mail';
+const hintPasswordAuthenticationView = 'Escolha sua senha';
+const hintCityAuthenticationView = 'Agora, sua cidade';
+const hintStreetAuthenticationView = 'Sua rua';
+const hintNeighborhoodAuthenticationView = 'Seu bairro';
+const hintNumberAuthenticationView = 'Número';
 
-// StepView
-const about = 'Se cuidar é uma forma de amar.\nCom o app Farmais, você compra '
-    'seus remédios, com receita ou sem, e recebe na sua casa. Sem precisar '
-    'se estressar em uma fila gigante.';
-const buttonNext = 'Prosseguir';
+// StepperView
+const aboutStepperView = 'Se cuidar é uma forma de amar.\nCom o app Farmais, você compra '
+    'seus remédios, com receita ou sem, e recebe em sua casa.\n'
+    'Pare de se estressar em filas.';
+const buttonNextStepperView = 'Prosseguir';
