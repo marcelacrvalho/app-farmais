f348488d11169fa8b5a77395427cea4e6dbad8e7
marcelacrvalho <marcelacrv@icloud.com>
2023-04-18 18:43:17 -0300

    Implementa usecase Authentication

--- lib/farmais/features/authentication/domain/usecases/authentication_usecase.dart
+++ lib/farmais/features/authentication/domain/usecases/authentication_usecase.dart
@@ -1,9 +1,20 @@
-import '../../../../utils/entities/user_entity.dart';
+import 'package:dartz/dartz.dart';
 
-class AuthenticationUseCase {
-  //TODO: receber um repository
+import '../../../../core/entities/user_entity.dart';
+import '../../../../core/errors/exceptions.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../entities/params/authentication_params.dart';
+import '../repositories/i_authentication_repository.dart';
 
-  Future<UserEntity>? auth() {
-    return null;
+class Authentication implements UseCase<UserEntity, AuthenticationParams> {
+  final IAuthenticationRepository repository;
+
+  Authentication({required this.repository});
+
+  @override
+  Future<Either<ServerException, UserEntity>> call(
+      AuthenticationParams params) async {
+    final response = await repository.authenticate(params);
+    return response;
   }
 }
