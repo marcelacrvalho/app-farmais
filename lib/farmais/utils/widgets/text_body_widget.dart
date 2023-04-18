0e8b2391b30030aa22437771483705584d6477f8
marcelacrvalho <marcelacrv@icloud.com>
2023-04-18 13:51:05 -0300

    Cria MyTextBodyWidget

---
+++ lib/farmais/utils/widgets/text_body_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyTextBodyWidget extends StatelessWidget {
+  final String text;
+  final bool? isBodyLarge;
+  const MyTextBodyWidget({
+    Key? key,
+    required this.text,
+    this.isBodyLarge = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: isBodyLarge!
+          ? Theme.of(context).textTheme.bodyLarge
+          : Theme.of(context).textTheme.bodySmall,
+    );
+  }
+}
