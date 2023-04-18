817c7b75c8e84b7dc115ac73103955e00a0ce2bb
marcelacrvalho <marcelacrv@icloud.com>
2023-04-18 14:16:59 -0300

    Cria rota nomeada /stepper

--- lib/values/routes.dart
+++ lib/values/routes.dart
@@ -1,5 +1,7 @@
 import '../farmais/features/authentication/presentation/views/authentication_view.dart';
+import '../farmais/features/stepper/presentation/views/stepper_view.dart';
 
 final myRoute = {
+  '/stepper': (context) => const StepperView(),
   '/authentication': (context) => const AuthenticationView(),
 };
