044a4288ef73fe4e1ce82eaf4c1c9dad708e2187
marcelacrvalho <marcelacrv@icloud.com>
2023-04-18 15:22:03 -0300

    Inicializa AuthenticationView

--- lib/farmais/features/authentication/presentation/views/authentication_view.dart
+++ lib/farmais/features/authentication/presentation/views/authentication_view.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
+import '../../../../utils/widgets/elevated_button_widget.dart';
+
+/// [it comes after StepperView and it is showed once while user realm is valid]
 class AuthenticationView extends StatelessWidget {
   const AuthenticationView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return Column(
+      children: [
+        MyElevatedButtonWidget.widthInfinity(
+          textButton: 'Entrar',
+          onPressed: () {},
+        ),
+      ],
+    );
   }
 }
