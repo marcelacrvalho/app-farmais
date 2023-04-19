eb9215bdca0a16e7b7e48e29494e21f29f5237c8
marcelacrvalho <marcelacrv@icloud.com>
2023-04-19 13:02:40 -0300

    Adiciona variável user a classe AuthenticateUser

--- lib/farmais/features/authentication/presentation/bloc/authentication_event.dart
+++ lib/farmais/features/authentication/presentation/bloc/authentication_event.dart
@@ -1,4 +1,13 @@
+import 'package:farmais/farmais/core/entities/user_entity.dart';
+
 abstract class AuthenticationEvent {}
-class AuthenticateUser extends AuthenticationEvent {}
+
 class LoadAuthentication extends AuthenticationEvent {}
+
+class AuthenticateUser extends AuthenticationEvent {
+  UserEntity user;
+
+  AuthenticateUser({required this.user});
+}
+
 class ErrorAuthentication extends AuthenticationEvent {}
