3d0ab0a7ba09d33b3469e26d6d2dea39ad037f8b
marcelacrvalho <marcelacrv@icloud.com>
2023-04-19 13:04:53 -0300

    Inicializa main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:farmais/farmais/features/stepper/presentation/views/stepper_view.dart';
 import 'package:flutter/material.dart';
 
 import 'values/routes.dart';
+import 'values/strings.dart';
+import 'values/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,16 +12,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // TODO: inicializar os bindings necessários WidgetBinding.ensureInitialized e HiveConfig.start()
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Farmais',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/splash',
+      title: title,
       routes: myRoute,
+      theme: myTheme,
+      home: const StepperView(),
     );
   }
 }
