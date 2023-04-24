f7dba815d2e17c136fb5c6877e33422cfa33100b
marcelacarvalho <marcelacrv@icloud.com>
2023-04-24 11:16:54 -0300

    Altera nome do usecase

--- lib/farmais/features/authentication/domain/usecases/authenticate_usecase.dart
+++ lib/farmais/features/authentication/domain/usecases/authenticate_usecase.dart
@@ -1,20 +1,19 @@
 import 'package:dartz/dartz.dart';
 
+import '../../../../core/entities/error_entity.dart';
 import '../../../../core/entities/user_entity.dart';
-import '../../../../core/errors/exceptions.dart';
 import '../../../../core/usecases/usecase.dart';
 import '../entities/params/authentication_params.dart';
 import '../repositories/i_authentication_repository.dart';
 
-class Authentication implements UseCase<UserEntity, AuthenticationParams> {
+class AuthenticateUser implements UseCase<UserEntity, AuthenticationParams> {
   final IAuthenticationRepository repository;
 
-  Authentication({required this.repository});
+  AuthenticateUser({required this.repository});
 
   @override
-  Future<Either<ServerException, UserEntity>> call(
+  Future<Either<ErrorEntity, UserEntity>> call(
       AuthenticationParams params) async {
-    final response = await repository.authenticate(params);
-    return response;
+    return await repository.authenticate(params.user);
   }
 }
